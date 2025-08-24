b36f2436e0355f77796eaad206617f299a199bf7
Rahul Sahu <rahulsahu79998@gmail.com>
2025-08-24 10:43:53 +0530

    add clerk authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
 import type { Metadata } from "next";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -23,12 +31,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <SignedOut>
+            <SignInButton />
+            <SignUpButton>
+              <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                Sign Up
+              </button>
+            </SignUpButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
           <h1 className="text-xl md:text-4xl lg:text-5xl font-medium">Your Personal AI Interviewer</h1>
           <p className="mt-4">Practice real, job-specific interviews, get instant feedback, and improve with AI-driven insights.</p>
           <section className="mt-4">
-            <Button>Get Started Free</Button>
+            <Link href="/login"><Button>Get Started Free</Button></Link>
             <Button variant="secondary" className="ml-2">Sign In</Button>
           </section>
         </header>
